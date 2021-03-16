e349c72b595e07a038e91eb5a103287a46281ee7
VladimirTroshev <troshevvladimir@gmail.com>
2021-03-16 21:08:09 +0300

    delet tests

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import Center from './Graph/centerPoint.js'
 function Application() {
 	return (
 		<div>
-			<h1>Hello from desctop another</h1>
-			<h2>Hello from laptop </h2>
 			<Center color="red"></Center>
 		</div>
 	)
