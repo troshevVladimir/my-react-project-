62feab5a814b091980d0ba77bbe90baa664eef9c
VladimirTroshev <troshevvladimir@gmail.com>
2021-03-26 15:21:22 +0300

    from begining

---
+++ src/Graph/Center.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default class Center extends React.Component {
+    render(){
+        return (
+            <div className="center point">
+
+            </div>
+        )
+    }
+}
